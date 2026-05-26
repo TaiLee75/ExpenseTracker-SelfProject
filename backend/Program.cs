6fd20c93df4b669bf728a12bbbbafa880e2ddffb
Tai Le <minhtai130tgm@gmail.com>
2026-05-26 23:30:40 +0700

    Enable Swagger in production

--- backend/Program.cs
+++ backend/Program.cs
@@ -108,11 +108,10 @@ using (var scope = app.Services.CreateScope())
 }
 
 //Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 if (app.Environment.IsDevelopment())
 {
